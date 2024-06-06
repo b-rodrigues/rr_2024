97e826f2078d992c6dab0343f1076bb0835cab31
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-06-06 11:53:00 +0200

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -3,14 +3,14 @@ library(tarchetypes)
 
 list(
   tar_force(
-    name = paper,
+    name = pres,
     command = quarto::quarto_render("presentation/pres_rr_2024.qmd"),
     format = "file", 
     force = TRUE
   ),
 
   tar_force(
-    name = paper,
+    name = tuto,
     command = quarto::quarto_render("tuto/tuto_rr_2024.qmd"),
     format = "file", 
     force = TRUE
